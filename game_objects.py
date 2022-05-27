2d26fadbb595a4fb44c6416e31e9c1a7d884087f
Jake <31741096+ZiarayZ@users.noreply.github.com>
2022-05-27 11:17:05 +0100

    Update game_objects.py

--- game_objects.py
+++ game_objects.py
@@ -1,42 +1,28 @@
 from random import randint
 import pygame
 
-class Wall(object):
-    def __init__(self,wx,wy): #Background,Climbable
+class Box(object):
+    def __init__(self,wx,wy,version="Surface"):
         self.rect = pygame.Rect(wx,wy,10,10)
-        self.colour = (randint(130,150),randint(60,80),randint(10,30))
+        if version == "Wall": #Background, Climbable
+            self.colour = (randint(130,150),randint(60,80),randint(10,30))
+        elif version == "Surface": #Foreground, Collidable
+            self.colour = (randint(0,20),randint(90,110),randint(0,20))
+        elif version == "Door": #Foreground, Collidable
+            self.colour = (randint(120,130),randint(120,130),randint(120,130))
 
-    def reset_wall(self):
+    def reset_self(self):
         self.active = False
 
-class Surface(object):
-    def __init__(self,wx,wy): #Foreground,Collidable
-        self.rect = pygame.Rect(wx,wy,10,10)
-        self.colour = (randint(0,20),randint(90,110),randint(0,20))
-
-    def reset_surface(self):
-        self.active = False
-
-class Door(object):
-    def __init__(self,wx,wy): #Foreground,Collidable
-        self.rect = pygame.Rect(wx,wy,10,10)
-        self.colour = (randint(120,130),randint(120,130),randint(120,130))
-
-    def reset_door(self):
-        self.active = False
-
-class Jump(object):
-    def __init__(self,wx,wy):
-        self.rect = pygame.Rect(wx,wy+8,10,2)
-        self.colour = (100,128,255)
-
-    def reset_jump(self):
-        self.active = False
-
-class Run(object):
-    def __init__(self,wx,wy):
-        self.rect = pygame.Rect(wx,wy+16,10,2)
-        self.colour = (255,128,100)
-
-    def reset_run(self):
+class Pad(object):
+    def __init__(self,wx,wy,version="Jump"):
+        if version == "Jump":
+            editVal = 8
+            self.colour = (100,128,255)
+        elif version == "Run":
+            editVal = 16
+            self.colour = (255,128,100)
+        self.rect = pygame.Rect(wx,wy+editVal,10,2)
+     
+    def reset_self(self):
         self.active = False
